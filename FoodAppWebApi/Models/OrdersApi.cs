61fe387e36ea8c54599c8abf3d1d62bbd261d0c5
PrasanthVempadapu <prasanthvempadapu@gmail.com>
2023-01-02 12:50:32 +0530

    Initial Commit

---
+++ FoodAppWebApi/Models/OrdersApi.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodAppWebApi.Models
+{
+    public class OrdersApi
+    {
+        public int InVoiceNo { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNo { get; set; }
+        public DateTime OrderTime { get; set; }
+    }
+}
---
+++ FoodAppWebApi/bin/Debug/net6.0/publish/Procfile
@@ -0,0 +1,1 @@
+web: dotnet exec ./FoodAppWebApi.dll --urls http://0.0.0.0:5000/
